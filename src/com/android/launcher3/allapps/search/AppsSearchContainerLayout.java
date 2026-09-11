CalyxOS/platform_packages_apps_Launcher3

Launcher3: Don't double-apply top inset to All Apps search bar
The All Apps container already reserves the status bar inset via its
padding (allAppsProfile.padding.top == insets.top), and then
InsettableFrameLayout.dispatchInsets() applies the same inset again
as the search bar's topMargin.

Only apply the top inset as a margin when the search bar is floating
(not wired up in AOSP) or on large screens, where the padding is
already fine.

After guarding mlp.topMargin, the drawer pill was too close to the
search bar on phones, so add 8dp of padding to make it symmetrical.

Change-Id: I334970c47baad6248805dc4bd1be61a74207766c

src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -198,7 +198,10 @@ public class AppsSearchContainerLayout extends ExtendedEditText
     @Override
     public void setInsets(Rect insets) {
         MarginLayoutParams mlp = (MarginLayoutParams) getLayoutParams();
-        mlp.topMargin = insets.top;
+        if (mAppsView.getSearchUiDelegate().isSearchBarFloating()
+                || mLauncher.getDeviceProfile().getDeviceProperties().isLargeScreen()) {
+            mlp.topMargin = insets.top;
+        }
         requestLayout();
     }
 

AresOS-AOSP/android_packages_apps_Launcher3

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -260,7 +260,10 @@ public class AppsSearchContainerLayout extends ExtendedEditText
     @Override
     public void setInsets(Rect insets) {
         MarginLayoutParams mlp = (MarginLayoutParams) getLayoutParams();
-        mlp.topMargin = insets.top;
+        if (mAppsView.getSearchUiDelegate().isSearchBarFloating()
+                || mLauncher.getDeviceProfile().getDeviceProperties().isTablet()) {
+            mlp.topMargin = insets.top;
+        }
         requestLayout();
     }
 